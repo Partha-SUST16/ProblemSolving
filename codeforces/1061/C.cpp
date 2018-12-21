bcb50f7de9dbb200417a4c738f25135853573364
Partha Protim Paul <partha05@student.sust.edu>
2018-12-22 00:19:00 +0600

    Add solution for problem `C - Multiplicity` Link: https://codeforces.com/contest/1061/problem/C Tags: data structures, dp, implementation, math, number theory, *1700 Ref: https://codeforces.com/contest/1061/submission/47343222

---
+++ codeforces/1061/C.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <set>
+#include <queue>
+#include <map>
+#include <cstdio>
+#include <cstring>
+#include <string>
+#include <stack>
+#include <algorithm>
+#include <limits.h>
+#include <bits/stdc++.h>
+#define fw(x) freopen("x.txt","w",stdout)
+#define For(i,a,b,c) for(int i = a;i < b;i+=c)
+#define REP(i,n) for(int i = 0;i < n;++i)
+#define REP1(i,n) for(int i = 1; i<=n;i++)
+#define mem(a,s) memset(a,s,sizeof a)
+#define fast ios_base::sync_with_stdio(0);cin.tie(NULL)
+#define pf printf
+#define sc scanf
+#define ll long long
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define all(v) v.begin(),v.end()
+#define vii vector<int>
+#define vll vector<ll>
+#define vss vector<string>
+#define debug(x)  cout<<#x"="<<(x)<<endl
+#define pb push_back
+
+
+using namespace std;
+
+
+// moves
+
+//int dx[]= {0,0,1,-1};/*4 side move*/
+//int dy[]= {-1,1,0,0};/*4 side move*/
+//int dx[]= {1,1,0,-1,-1,-1,0,1};/*8 side move*/
+//int dy[]= {0,1,1,1,0,-1,-1,-1};/*8 side move*/
+//int dx[]={1,1,2,2,-1,-1,-2,-2};/*knight move*/
+//int dy[]={2,-2,1,-1,2,-2,1,-1};/*knight move*/
+
+
+//big_mod
+
+//ll bigmod(ll a,ll b,ll m)
+//{if(b == 0) return 1%m;ll x = bigmod(a,b/2,m);x = (x * x) % m;if(b % 2 == 1) x = (x * a)% m;return x;}
+//ll BigMod(ll B,ll P,ll M){ ll R=1%M; while(P>0) {if(P%2==1){R=(R*B)%M;}P/=2;B=(B*B)%M;} return R;} /// (B^P)%M
+
+//ll getBit(ll num, int idx) {return ((num >> idx) & 1ll) == 1ll;}
+//ll setBit1(ll num, int idx) {return num or (1ll<<idx);}
+//ll setBit0(ll num, int idx) {return num & ~(1ll<<idx);}
+//ll flipBit(ll num, int idx) {return num ^ (1ll<<idx);}
+
+const int mod = 1000000007;
+
+ll dp[1000010];
+vii v,p;
+int main()
+{
+    int n,x;sc("%d",&n);
+    dp[0]=1;
+    REP(i,n)
+    {
+        sc("%d",&x);
+        v.clear(),p.clear();
+
+        for(int j=1;j*j<=x;j++)
+        {
+            if(j*j==x)v.pb(j);
+            else if(x%j==0)
+            {
+                v.pb(j);p.pb(x/j);
+            }
+        }
+        for(int j:p)
+        {
+            dp[j]+=dp[j-1];
+            dp[j]%=mod;
+        }
+        for(int j=v.size()-1;j>=0;j--)
+        {
+            dp[v[j]]+=dp[v[j]-1];
+            dp[v[j]]%=mod;
+        }
+    }
+    for(int i=2;i<=n;i++)
+        dp[i]+=dp[i-1],dp[i]%=mod;
+    cout<<dp[n]<<endl;
+    return 0;
+}
