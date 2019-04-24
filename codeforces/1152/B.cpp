e475f1072853f84c3ceb9eac8cfc7fdabdcd1488
Partha Protim Paul <partha05@student.sust.edu>
2019-04-24 21:47:00 +0600

    Add solution for problem `B - Neko Performs Cat Furrier Transform` Link: https://codeforces.com/contest/1152/problem/B Tags: bitmasks, constructive algorithms, dfs and similar, math, *1300 Ref: https://codeforces.com/contest/1152/submission/53245533

---
+++ codeforces/1152/B.cpp
@@ -0,0 +1,116 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <set>
+#include <queue>
+#include <map>
+#include <cstdio>
+#include <cstring>
+#include <string>
+#include <stack>
+#include <algorithm>
+#include <limits.h>
+#include <bits/stdc++.h>
+#define fw(x) freopen("x.txt","w",stdout)
+#define For(i,a,b,c) for(int i = a;i < b;i+=c)
+#define REP(i,n) for(int i = 0;i < n;++i)
+#define REP1(i,n) for(int i = 1; i<=n;i++)
+#define mem(a,s) memset(a,s,sizeof a)
+#define fast ios_base::sync_with_stdio(0);cin.tie(NULL)
+#define pf printf
+#define sc scanf
+#define ll long long
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define all(v) v.begin(),v.end()
+#define vii vector<int>
+#define vll vector<ll>
+#define vss vector<string>
+#define debug(x)  cout<<#x"="<<(x)<<endl
+#define pb push_back
+
+
+using namespace std;
+
+
+// moves
+
+//int dx[]= {0,0,1,-1};/*4 side move*/
+//int dy[]= {-1,1,0,0};/*4 side move*/
+//int dx[]= {1,1,0,-1,-1,-1,0,1};/*8 side move*/
+//int dy[]= {0,1,1,1,0,-1,-1,-1};/*8 side move*/
+//int dx[]={1,1,2,2,-1,-1,-2,-2};/*knight move*/
+//int dy[]={2,-2,1,-1,2,-2,1,-1};/*knight move*/
+
+
+//big_mod
+
+//ll bigmod(ll a,ll b,ll m)
+//{if(b == 0) return 1%m;ll x = bigmod(a,b/2,m);x = (x * x) % m;if(b % 2 == 1) x = (x * a)% m;return x;}
+//ll BigMod(ll B,ll P,ll M){ ll R=1%M; while(P>0) {if(P%2==1){R=(R*B)%M;}P/=2;B=(B*B)%M;} return R;} /// (B^P)%M
+
+//ll getBit(ll num, int idx) {return ((num >> idx) & 1ll) == 1ll;}
+//ll setBit1(ll num, int idx) {return num or (1ll<<idx);}
+//ll setBit0(ll num, int idx) {return num & ~(1ll<<idx);}
+//ll flipBit(ll num, int idx) {return num ^ (1ll<<idx);}
+
+const int mod = 1000000007;
+//ll qpow(ll n,ll k) {ll ans=1;assert(k>=0);n%=mod;while(k>0){if(k&1) ans=(ans*n)%mod;n=(n*n)%mod;k>>=1;}return ans%mod;}
+
+ll db(ll x);
+int isP(ll x);
+ll n;
+vll arr;
+ll c;
+void work()
+{
+    if(c==0)
+    {
+        cout<<0<<endl;
+        exit(0);
+    }
+    cout<<c<<endl;
+    for(ll i:arr)cout<<i<<" ";
+}
+int main()
+{
+    cin >> n;
+    while(!isP(n+1))
+    {
+        ll t = db(n);
+        arr.pb(t);
+        t = pow(2,t)-1;
+        n = ((n|t)&(~n|~t));
+        c++;
+        if(isP(n+1))
+            break;
+        n++,c++;
+    }
+    work();
+    return 0;
+}
+ll db(ll x)
+{
+    ll bin[32]={0};
+    for(int i = 31; i >= 0; i--)
+    {
+        ll temp = n >> i;
+        if (temp & 1)bin[i]=1;
+    }
+    ll h=0;
+    for(ll i=31;i>=0;i--)
+    {
+    	if(bin[i]==1)h++;
+    	else
+    	{
+    		if(h>=1)return i+1;
+    	}
+    }
+}
+int isP(ll x)
+{
+     if(x==0)
+        return 0;
+
+   return (ceil(log2(x)) == floor(log2(x)));
+}
